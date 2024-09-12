4c97ad4c5d7c28134a8589ee2dda0d873007639d
Mariam <mari.kifshidze@gmail.com>
2024-09-12 19:18:43 +0400

    Write Line

--- ConsoleAppIntrodaction/Program.cs
+++ ConsoleAppIntrodaction/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+Console.WriteLine("I am Learning C#");
+Console.WriteLine(3 + 3);
+Console.WriteLine("Hello " + "World");
