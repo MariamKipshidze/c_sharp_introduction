c5c4ad786c13c77e66ffea5c69cff099b858bc98
Mariam <mari.kifshidze@gmail.com>
2024-09-13 22:52:34 +0400

    Add bool, double and char variables

--- ConsoleAppIntrodaction/variable.cs
+++ ConsoleAppIntrodaction/variable.cs
@@ -8,3 +8,15 @@ Console.WriteLine(myNum);
 
 myNum = 20;
 Console.WriteLine(myNum);
+
+// Double
+double myDoubleNum = 5.99D;
+Console.WriteLine(myDoubleNum);
+
+// Char
+char myLetter = 'D';
+Console.WriteLine(myLetter);
+
+// Bool
+bool myBool = true;
+Console.WriteLine(myBool);
